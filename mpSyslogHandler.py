a2f21234bf90cd0f38031a3c69d902cc3ee01fcb
Chris Moyer <cmoyer@newstex.com>
2014-06-18 10:06:10 -0400

    Initial checkin

---
+++ mpSyslogHandler.py
@@ -0,0 +1,63 @@
+"""
+@Author: Chris Moyer <cmoyer@newstex.com>
+@See: http://stackoverflow.com/questions/641420/how-should-i-log-while-using-multiprocessing-in-python
+
+Multi-Processing Syslog handler,
+because the Syslog handler does not handle MultiProcessing
+very well, this overrides the Syslog handler and makes sure
+the emit function is called properly
+
+"""
+from logging.handlers import SysLogHandler
+import multiprocessing, threading, sys, traceback
+
+class MultiProcessingLog(SysLogHandler):
+	"""A special SysLog MultiProcessing handler, which
+	allows us to make SysLog work with multiple processes
+	calling the log function at once"""
+
+	def __init__(self,*args, **kwargs):
+		SysLogHandler.__init__(self, *args, **kwargs)
+		self.queue = multiprocessing.Queue(-1)
+
+		t = threading.Thread(target=self.receive)
+		t.daemon = True
+		t.start()
+
+	def receive(self):
+		while True:
+			try:
+				record = self.queue.get()
+				SysLogHandler.emit(self, record)
+			except (KeyboardInterrupt, SystemExit):
+				raise
+			except EOFError:
+				break
+			except:
+				traceback.print_exc(file=sys.stderr)
+
+	def send(self, s):
+		self.queue.put_nowait(s)
+
+	def _format_record(self, record):
+		"""ensure that exc_info and args
+		have been stringified.  Removes any chance of
+		unpickleable things inside and possibly reduces
+		message size sent over the pipe"""
+		if record.args:
+			record.msg = record.msg % record.args
+			record.args = None
+		if record.exc_info:
+			self.format(record)
+			record.exc_info = None
+
+		return record
+
+	def emit(self, record):
+		try:
+			s = self._format_record(record)
+			self.send(s)
+		except (KeyboardInterrupt, SystemExit):
+			raise
+		except:
+			self.handleError(record)
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2014, Chris Moyer <cmoyer@newstex.com>
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# 'Software'), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+
+setup(name = 'mpSysLogHandler',
+	version = '0.1',
+	description = 'MultiProcess-safe SysLog Handler',
+	author = 'Chris Moyer',
+	author_email = 'cmoyer@newstex.com',
+	url = 'https://github.com/cmoyer/mpSysLogHandler/',
+	packages = ['mpSysLogHandler'],
+	license = 'MIT',
+	platforms = 'Posix; MacOS X; Windows',
+	classifiers = ['Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Topic :: Logging',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.5',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7'],
+	)
